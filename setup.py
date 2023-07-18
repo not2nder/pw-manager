fa3f1be273f3de5c7012ed0f6408377bbc3486f0
not2nder <130621173+not2nder@users.noreply.github.com>
2023-07-17 21:53:43 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 """
 EXECUTE ESTE SCRIPT APENAS NA PRIMEIRA INSTALAÇÃO
 """
-import os
 
 input("Vamos instalar toas as dependências Python \nAperte [ENTER] para prosseguir ")
 os.system("pip install -U -r requirements.txt")
